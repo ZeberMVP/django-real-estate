3955e1a7b84b2ff55b0adc12d078a5a1d05b023b
ZeberMVP <rubeen722@gmail.com>
2023-05-18 10:29:24 +0200

    add/login and logout

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import propertyReducer from "../features/properties/propertySlice";
 
 export const store = configureStore({
   reducer: {
     properties: propertyReducer,
+    auth: authReducer,
   },
 });
